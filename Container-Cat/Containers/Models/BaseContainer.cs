8c2168d7259c3b38fcbc8f5692c954a168bfb3c2
Okazaki Yumemi <suwako.moria@yandex.ru>
2023-06-05 09:18:24 +0300

    Merged Port and Mount into BaseContainer class file

--- Container-Cat/Containers/Models/BaseContainer.cs
+++ Container-Cat/Containers/Models/BaseContainer.cs
@@ -1,10 +1,34 @@
-﻿namespace Container_Cat.Containers.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Container_Cat.Containers.Models
 {
     public class BaseContainer
     {
+        [Key]
         public string Id { get; set; }
         public string State { get; set; }
-        public string[] Names { get; set; }
+        public List<string> Names { get; set; }
         public string Image { get; set; }
+        public List<Port> Ports { get; set; }
+        public List<Mount> Mounts { get; set; }
+    }
+    public class Port
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string IP { get; set; }
+        public int PrivatePort { get; set; }
+        public int PublicPort { get; set; }
+        public string Type { get; set; }
+    }
+    public class Mount
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public bool RW { get; set; }
     }
 }
