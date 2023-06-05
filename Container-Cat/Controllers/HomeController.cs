d48c241ad18860ecea3f1bfd0e1e77c3bcdd586b
Okazaki Yumemi <suwako.moria@gmail.com>
2023-06-05 08:15:08 +0300

    Preparing for connecting to db (SQLite)

--- Container-Cat/Controllers/HomeController.cs
+++ Container-Cat/Controllers/HomeController.cs
@@ -18,29 +18,6 @@ namespace Container_Cat.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            SystemDataGathering dataGatherer = new SystemDataGathering();
-            SystemOperations<DockerContainer> DockerSystems = new SystemOperations<DockerContainer>();
-            SystemOperations<PodmanContainer> PodmanSystems;
-            List<HostAddress> Hosts = new List<HostAddress>()
-            {
-                new HostAddress("127.0.0.1", "3375"),
-                new HostAddress("192.168.56.99", "3375"),
-                new HostAddress("192.168.0.104", "3375"),
-                new HostAddress("google.com", "80") 
-            };
-            await foreach (var dataObj in dataGatherer.FetchDataObjectRangeAsync(Hosts))
-            {
-                if (dataObj.InstalledContainerEngines == ContainerEngine.Docker)
-                {
-                    DockerSystems.AddHostSystem(new HostSystem<DockerContainer>(dataObj));
-                    Console.WriteLine($"{dataObj.NetworkAddress.Ip}:{dataObj.NetworkAddress.Port} is {dataObj.InstalledContainerEngines}.");
-                    Console.WriteLine($"There are {DockerSystems.SystemsCount()} systems with Docker.");
-                }
-                else if (dataObj.InstalledContainerEngines == ContainerEngine.Podman)
-                {
-                    throw new NotImplementedException("Podman is not implemented.");
-                } 
-            }
             //var tasks = Hosts.Select(async host =>
             //{
             //    SystemDataObj item = new SystemDataObj(host);
