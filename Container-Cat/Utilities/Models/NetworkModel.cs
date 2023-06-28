f12c55cd755001cb793c0ef657c1e4f3a4001a1c
Okazaki Yumemi <suwako.moria@yandex.ru>
2023-06-28 11:30:29 +0300

    NetworkModel code cleanup

--- Container-Cat/Utilities/Models/NetworkModel.cs
+++ Container-Cat/Utilities/Models/NetworkModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Container_Cat.Utilities.Models
+﻿using Container_Cat.Containers.Models;
+
+namespace Container_Cat.Utilities.Models
 {
     public class HostAddress
     {
@@ -13,47 +15,13 @@
             {
                 if (value == null || value.Length < 1)
                 {
-                    Console.WriteLine("Null or empty value of Hostname.Ip passed.");
                     hostname = "";
                 }
                 else
                     hostname = value;
             }
-            /*
-            get { return Ip; }
-            set
-            {
-                string _ip = value;
-                var addressParts = _ip.Split('.');
-                if (addressParts.Length != 4) throw new ArgumentException("The passed IP address was in incorrect format.");
-                foreach (var part in addressParts)
-                {
-                    byte _ipPart;
-                    if (Byte.TryParse(part, out _ipPart))
-                    {
-                        if (!((_ipPart >= 0) && (_ipPart <= 255))) throw new ArgumentOutOfRangeException("The passed IP address fragment had incorrect value.");
-                    }
-                    else throw new ArgumentException("Unable to parse passed IP address fragment to Byte.");
-                }
-                Ip = value;
-            }
-            */
         }
-        public string? Port { get; set;
-            /*
-            get { return Port; }
-            set
-            {
-                Int32 _port;
-                if (Int32.TryParse(value, out _port))
-                {
-                    if ((_port > 0) && (_port <= 65535)) Port = value;
-                    else throw new ArgumentOutOfRangeException("The passed Port number was succesfully converted to Int32, but it's value was incorrect.");
-                }
-                else throw new ArgumentException("Unable to parse passed Port number to Int32");
-            }
-            */
-        }
+        public string? Port { get; set; }
 
         public HostAddress(string _ip, string _port)
         {
